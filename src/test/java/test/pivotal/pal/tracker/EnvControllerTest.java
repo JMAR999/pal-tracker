1a78f73f0a44ab00e0313bd3d42f5f2da02ee7d7
John Martin <jmartin@Johns-MacBook-Air.local>
2019-02-26 17:38:17 -0500

    User can now see certain environment variables

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,23 +1,25 @@
 package test.pivotal.pal.tracker;
 
+
+import io.pivotal.pal.tracker.EnvController;
 import org.junit.Test;
 
 import java.util.Map;
-import io.pivotal.pal.tracker.EnvController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
+
     @Test
     public void getEnv() throws Exception {
-        EnvController controller = new EnvController(
-            "8675",
-            "12G",
+        EnvController controller = new EnvController("8675",
+                "12G",
             "34",
             "123.sesame.street"
         );
 
         Map<String, String> env = controller.getEnv();
+
 
         assertThat(env.get("PORT")).isEqualTo("8675");
         assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
